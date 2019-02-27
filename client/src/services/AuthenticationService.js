13dcae453d706f7a923f7cf9a0560b36bc115f40
cristian6connex <cristian.gutierrez@6connex>
2019-02-27 10:53:36 -0500

    partial commit

--- client/src/services/AuthenticationService.js
+++ client/src/services/AuthenticationService.js
@@ -5,8 +5,3 @@ export default {
     return Api().post('register', credentials)
   }
 }
-
-// AthenticationService.register({
-//     email: 'testing@gmail.com'
-//     password: '123456'
-// })
--- server/src/app.js
+++ server/src/app.js
@@ -8,6 +8,10 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/register', (req, res) => { res.send({ message: `Hello ${req.body.email}! Your user was Registered!` }) })
+app.post('/register', (req, res) => {
+  res.send({
+    message: `Hello ${req.body.email}! Your user was Registered!`
+  })
+})
 
 app.listen(process.env.PORT || 8081)
